7e5dcbfecf279a08dfd05d0783a7c702b405e7bc
hitomi.abe <hitomi.a486@gmail.com>
2020-09-04 10:48:39 +0000

    fixed based on 3rd review

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,5 +54,8 @@ class TasksController < ApplicationController
   
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to login_path
+    end 
   end 
 end
